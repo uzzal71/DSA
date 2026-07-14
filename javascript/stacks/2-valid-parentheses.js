c76473843189795bd544928e482e2275ac300e52
uzzal71 <uzzalroy.acm@gmail.com>
2026-07-14 19:24:18 +0600

    brackets validation

--- javascript/stacks/2-valid-parentheses.js
+++ javascript/stacks/2-valid-parentheses.js
@@ -19,3 +19,46 @@
 
 // Input: s = "{[]}"
 // Output: true
+
+function isValid(s) {
+  const stack = [];
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+    } else if (char === ")" || char === "}" || char === "]") {
+      if (isEmpty(stack)) {
+        return false;
+      }
+
+      const top = stack.pop();
+      if (
+        (char === ")" && top !== "(") ||
+        (char === "}" && top !== "{") ||
+        (char === "]" && top !== "[")
+      ) {
+        return false;
+      }
+    }
+  }
+  return isEmpty(stack);
+}
+
+function isEmpty(stack) {
+  return stack.length === 0;
+}
+
+const string1 = "()";
+const string2 = "()[]{}";
+const string3 = "(]";
+const string4 = "([)]";
+const string5 = "{[]}";
+
+console.log(isValid(string1)); // Output: true
+console.log(isValid(string2)); // Output: true
+console.log(isValid(string3)); // Output: false
+console.log(isValid(string4)); // Output: false
+console.log(isValid(string5)); // Output: true
+
+// Time Complexity: O(n), where n is the length of the input string s. We iterate through each character in the string once.
+// Space Complexity: O(n), where n is the length of the input string s. In the worst case, we may need to store all opening brackets in the stack.
